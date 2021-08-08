82bef388dea4c250cd1031c12868741eb6665ed2
srkrishna3384 <srkrishna3384@gmail.com>
2021-08-08 11:53:06 +0530

    Added TestDemo Project

---
+++ src/test/java/com/testdemo/TestOne.java
@@ -0,0 +1,14 @@
+package com.testdemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestOne {
+	public static void demoOne() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.rameshsoft.com");
+	}
+}
